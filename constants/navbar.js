b6bed72d0baa405ef34fbf5cb0a01d40e96ab3a2
Mihael Barešić <73283548+MihaelBaresic@users.noreply.github.com>
2022-12-14 22:51:15 +0100

    Login functionality

---
+++ constants/navbar.js
@@ -0,0 +1,8 @@
+export const navigationItems = [
+    { label: 'Home', path: '/home' },
+    { label: 'About us', path: '/about' },
+    { label: 'Showcase', path: '' },
+    { label: 'Blog', path: '' },
+    { label: 'Sign in', path: '/login' },
+    { label: 'Me', path: '/self', needsAuth: true },
+];

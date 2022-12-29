7fad02ea49b8543bee69816edba0c4300a4ab175
JakovJonjić <73243731+Jakov69@users.noreply.github.com>
2022-12-29 11:32:56 +0100

    SVGS

---
+++ components/SVG.js
@@ -0,0 +1,44 @@
+import styles from '../styles/SVG.module.css'
+
+const SVG = () => (
+  <section className={`${styles.SVG_section} ${styles.container}`}>
+    <div className={styles.SVG_txt_holder}>We work with</div>
+    <div className={styles.flex_holder_svg}>
+      <div className={styles.svg_img_holder}>
+        <img className={styles.svg_img} src="/birin.svg" height='216px' width='231px'/> 
+      </div>
+      <div className={styles.svg_img_holder}>
+        <img className={styles.svg_img} src="/roguljic.svg" height='216px' width='231px'/> 
+      </div>
+      <div className={styles.svg_img_holder}>
+        <img className={styles.svg_img} src="/bubicic.svg" height='216px' width='231px'/> 
+      </div>
+      <div className={styles.svg_img_holder}>
+        <img className={styles.svg_img} src="/pensa.svg" height='216px' width='231px'/> 
+      </div>
+    </div>
+    <div className={styles.SVG_txt_holder}>Quality guarantees</div>
+    <div className={styles.flex_holder_svg}>
+      <div className={styles.svg_img_holder}>
+        <img className={styles.svg_img} src="/euIzvornost.png" height='200px' width='200px'/> 
+      </div>
+      <div className={styles.svg_img_holder}>
+        <img className={styles.svg_img} src="/zemljopisno_pod.png" height='200px' width='200px'/> 
+      </div>
+      <div className={styles.svg_img_holder}>
+        <img className={styles.svg_img} src="/srednji.svg" height='200px' width='200px'/> 
+      </div>
+      <div className={styles.svg_img_holder}>
+        <img className={styles.svg_img} src="/meso.svg" height='200px' width='200px'/> 
+      </div>
+      <div className={styles.svg_img_holder}>
+        <img className={styles.svg_img} src="/eko.svg" height='200px' width='200px'/> 
+      </div>
+      <div className={styles.svg_img_holder}>
+        <img className={styles.svg_img} src="/med.svg" height='200px' width='200px'/> 
+      </div>
+    </div>
+  </section>
+);
+
+export default SVG;

382d11b4f7e0dd8089c29339192d08c73e0476cc
Mihael Barešić <73283548+MihaelBaresic@users.noreply.github.com>
2023-01-08 11:20:31 +0100

    Pre-final version

--- components/Cart.js
+++ components/Cart.js
@@ -88,15 +88,15 @@ const Cart = () => {
               
             <tr key={productId} className={styles.tr}>
               <td className={styles.td2} width='25%'><img src={product.imageUrl} alt={product.name}  width='150px' height='10%'/><div className={styles.td3}>{product.name}</div></td>      
-              <td className={styles.td}>${product.price.toFixed(2)}</td>
+              <td className={styles.td}>{product.price.toFixed(2)}€</td>
               <td className={styles.td}>
                 <div className={styles.qty_holder}>
-                  <button className={`${styles.btnssss} ${styles.btn_plus}`} onClick={() => increaseQuantity(product.id)}>+</button>
-                  <div className={styles.qty_holder2}>{product.quantity}</div>
                   <button className={`${styles.btnssss} ${styles.btn_minus}`} onClick={() => decreaseQuantity(product.id)}>-</button>
+                  <div className={styles.qty_holder2}>{product.quantity}</div>
+                  <button className={`${styles.btnssss} ${styles.btn_plus}`} onClick={() => increaseQuantity(product.id)}>+</button>
                 </div>
               </td>
-              <td className={styles.td}>${(product.price * product.quantity).toFixed(2)}</td>
+              <td className={styles.td}>{(product.price * product.quantity).toFixed(2)}€</td>
               <td className={styles.td}>
                 <button className={styles.remove_btn} onClick={() => removeProduct(product.id)}>✖</button>
               </td>
@@ -113,7 +113,7 @@ const Cart = () => {
       </div>
       <div className={`${styles.td4} ${styles.td4_2}`}>
         <div className={styles.total_finish}>TOTAL:</div>
-        <div className={styles.total_finish}>${total.toFixed(2)}</div>
+        <div className={styles.total_finish}>{total.toFixed(2)}€</div>
       </div>
       <div className={styles.td4}>
         <div className={styles.cta__full4}>CHECK OUT</div>
@@ -138,19 +138,19 @@ const Cart = () => {
             <div className={styles.mobile_cart_pricesss}>
               <div className={styles.total_mobile_1}>
                 <div className={styles.th_mobile}>Unit price:</div>
-                <div className={styles.td_mobile}>${product.price.toFixed(2)}</div>
+                <div className={styles.td_mobile}>{product.price.toFixed(2)}€</div>
               </div>
               <div className={styles.total_mobile_2}>
                 <div className={styles.th_mobile}>Total price:</div>
-                <div className={styles.td_mobile}>${(product.price * product.quantity).toFixed(2)}</div>
+                <div className={styles.td_mobile}>{(product.price * product.quantity).toFixed(2)}€</div>
               </div>
           </div>
           </div>
           <div className={styles.mobile_cart_qty}>
             <div className={styles.qty_holder_mobile}>
-              <button className={`${styles.btnssss} ${styles.btn_plus}`} onClick={() => increaseQuantity(product.id)}>+</button>
-              <div className={styles.qty_holder2}>{product.quantity}</div>
               <button className={`${styles.btnssss} ${styles.btn_minus}`} onClick={() => decreaseQuantity(product.id)}>-</button>
+              <div className={styles.qty_holder2}>{product.quantity}</div>
+              <button className={`${styles.btnssss} ${styles.btn_plus}`} onClick={() => increaseQuantity(product.id)}>+</button>
             </div>
           </div>
         </div>
@@ -163,7 +163,7 @@ const Cart = () => {
 <div className={styles.mobile_cart_btn_checkout}>
   <div className={`${styles.td4_mobile} ${styles.td4_mobile} `}>
     <div className={styles.total_finish_mobile}>TOTAL:</div>
-    <div className={styles.total_finish}>${total.toFixed(2)}</div>
+    <div className={styles.total_finish}>{total.toFixed(2)}€</div>
   </div>
   <div className={styles.td4_mobile}>
     <div className={styles.cta__full4}>CHECK OUT</div>
